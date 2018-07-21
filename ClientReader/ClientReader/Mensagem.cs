92e2e50fb2db6bfd1636a80badc76f005aee0444
Paulo Motta <prmottajr@gmail.com>
2018-07-21 16:08:16 -0300

    Added three methods to create different types of messages with the respective frame configuration. Added a Unit Test for the Mensagem creation methods

--- ClientReader/ClientReader/Mensagem.cs
+++ ClientReader/ClientReader/Mensagem.cs
@@ -8,5 +8,41 @@ namespace ClientReader
 {
     public class Mensagem
     {
+        
+
+        private Frame frame;
+
+        public Frame Frame
+        {
+            get
+            {
+                return frame;
+            }
+        }
+
+        private Mensagem(Frame f)
+        {
+            this.frame = f;
+        }
+
+        public static Mensagem createMensagemLerNumSerie()
+        {
+            Frame f = new Frame(0, (byte)Frame.CODE.LerNumSerie, null);
+            return new Mensagem(f);
+        }
+
+        public static Mensagem createMensagemLerStatus()
+        {
+            Frame f = new Frame(0, (byte)Frame.CODE.LerStatus, null);
+            return new Mensagem(f);
+        }
+
+        public static Mensagem createMensagemDefinirRegistro(UInt16 registro)
+        {
+            byte []data = BitConverter.GetBytes(registro);
+            Frame f = new Frame((byte)data.Length, (byte)Frame.CODE.DefinirRegistro, data);
+            return new Mensagem(f);
+        }
+        
     }
 }
---
+++ ClientReader/UnitTestClientReader/MensagemTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientReader.Tests
+{
+    [TestClass()]
+    public class MensagemTests
+    {
+        [TestMethod()]
+        public void createMensagemLerNumSerieTest()
+        {
+            Mensagem m = Mensagem.createMensagemLerNumSerie();
+            Assert.AreEqual(0x01,m.Frame.Checksum);
+            Assert.AreEqual((byte)Frame.CODE.LerNumSerie, m.Frame.Code);
+        }
+
+        [TestMethod()]
+        public void createMensagemLerStatusTest()
+        {
+            Mensagem m = Mensagem.createMensagemLerStatus();
+            Assert.AreEqual(0x02, m.Frame.Checksum);
+            Assert.AreEqual((byte)Frame.CODE.LerStatus, m.Frame.Code);
+        }
+
+        [TestMethod()]
+        public void createMensagemDefinirRegistroTest()
+        {
+            Mensagem m = Mensagem.createMensagemDefinirRegistro(380);
+            Assert.AreEqual(0x7C, m.Frame.Checksum);
+            Assert.AreEqual((byte)Frame.CODE.DefinirRegistro, m.Frame.Code);
+            Assert.AreEqual(2, m.Frame.Data.Length);
+            Assert.AreEqual(0x7C, m.Frame.Data[0]);
+            Assert.AreEqual(0x01, m.Frame.Data[1]);
+        }
+    }
+}
