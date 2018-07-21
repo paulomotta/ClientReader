600add49149d9c6291645454b518443d36e1686f
Paulo Motta <prmottajr@gmail.com>
2018-07-21 02:19:14 -0300

    Added a Medidor class to abstract the interaction with the remote device/server. Added a Mensagem class to help match commands with their responses.

---
+++ ClientReader/ClientReader/Medidor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientReader
+{
+    public class Medidor
+    {
+        private string ip;
+        private int porta;
+
+        public bool connect()
+        {
+            return false;
+        }
+
+        public bool disconnect()
+        {
+            return false;
+        }
+
+        public bool sendCommand(Mensagem cmd)
+        {
+            return false;
+        }
+
+        public Mensagem receive()
+        {
+            Mensagem msg = null;
+            validaRetorno(msg);
+            return null;
+        }
+
+        private void validaRetorno(Mensagem msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
