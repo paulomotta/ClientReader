deeb8c0c5bbe7842d00eb38f21f3997460c7c309
Paulo Motta <prmottajr@gmail.com>
2018-07-21 01:29:56 -0300

    Added Equals and HashCode for the Frame class, added two enums for clarity, added a method to pack the whole frame as a byte array, added a constructor to recreate a frame from a byte array, added a method to validate the received checksum with what it should be based on its calculation, added a method to calculate the frame size for packing, added corresponding tests to make sure the first conditions are covered

--- ClientReader/ClientReader/Frame.cs
+++ ClientReader/ClientReader/Frame.cs
@@ -8,6 +8,16 @@ namespace ClientReader
 {
     public class Frame
     {
+        public enum FIELDS : int { HEADER, LENGTH, CODE, DATA};
+        enum CODE : byte
+        {
+            LerNumSerie = 0x01,
+            LerStatus = 0x02,
+            DefinirRegistro = 0x03,
+            LerDataHora = 0x04,
+            LerValor = 0x05,
+            Erro = 0xFF
+        }
         private byte header = 0x7D;
         private byte length;
         private byte code;
@@ -62,6 +72,23 @@ namespace ClientReader
             this.checksum = createChecksum();
         }
 
+        public Frame(byte[] packedData)
+        {
+            this.header = packedData[(int)FIELDS.HEADER];
+            this.length = packedData[(int)FIELDS.LENGTH];
+            this.code = packedData[(int)FIELDS.CODE];
+            if (this.length > 0)
+            {
+                byte[] tmp = new byte[this.length];
+                Array.Copy(packedData, (int)FIELDS.DATA, tmp, 0, this.length);
+                this.data = tmp;
+            } else
+            {
+                this.data = null;
+            }
+            this.checksum = packedData[(int)FIELDS.DATA + this.length];
+        }
+
         public override string ToString()
         {
             return header + " " + length + " " + code + " " + data + " " + checksum;
@@ -83,6 +110,61 @@ namespace ClientReader
         public byte createChecksum()
         {
             return (byte) (length ^ code ^ xorData());
+        }
+
+        public bool isValidChecksum()
+        {
+            return (createChecksum() == this.checksum);
+        }
+
+        public int frameSize()
+        {
+            int size = 4;
+            if (data != null) { size += data.Length; }
+            return size;
+        }
+
+        public byte[] pack()
+        {
+            byte[] tmp = new byte[frameSize()];
+            tmp[(int)FIELDS.HEADER] = header;
+            tmp[(int)FIELDS.LENGTH] = length;
+            tmp[(int)FIELDS.CODE] = code;
+            if (data != null)
+            {
+                Array.Copy(data, 0, tmp, (int)FIELDS.DATA, length);
+            }
+            tmp[(int)FIELDS.DATA + length] = checksum;
+            return tmp;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Frame f = (Frame)obj;
+
+            if (data != null)
+            {
+                if (f.data == null) return false;
+                if (length != f.length) return false;
+
+                for (int i=0; i < length; i++)
+                {
+                    if (data[i] != f.data[i]) return false;
+                }
+            }
+
+            return (header == f.header) && 
+                (length == f.length) &&
+                (code == f.code) &&
+                checksum == f.checksum;
+        }
+
+        public override int GetHashCode()
+        {
+            return length+code+checksum;
         }
     }
 }
--- ClientReader/UnitTestClientReader/FrameTests.cs
+++ ClientReader/UnitTestClientReader/FrameTests.cs
@@ -23,7 +23,7 @@ namespace ClientReader.Tests
         [TestMethod()]
         public void createChecksumDataTest()
         {
-            byte []data = {0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x00};
+            byte[] data = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x00 };
             Frame f = new Frame(0x08, 0x81, data);
             byte expected = 0xC9;
             Assert.AreEqual(expected, f.createChecksum());
@@ -45,6 +45,100 @@ namespace ClientReader.Tests
             Frame f = new Frame(0x08, 0x81, data);
             byte expected = 0x40;
             Assert.AreEqual(expected, f.xorData());
+        }
+
+        [TestMethod()]
+        public void packTestNullData()
+        {
+            Frame f = new Frame(0x00, 0x01, null);
+            byte[] tmp = f.pack();
+            Console.WriteLine(tmp.Length);
+            Assert.AreEqual(4, tmp.Length);
+            Assert.AreEqual(0x7D, tmp[(int)Frame.FIELDS.HEADER]);
+            Assert.AreEqual(0x00, tmp[(int)Frame.FIELDS.LENGTH]);
+            Assert.AreEqual(0x01, tmp[(int)Frame.FIELDS.CODE]);
+            Assert.AreEqual(0x01, tmp[(int)Frame.FIELDS.DATA + f.Length]);
+        }
+
+        [TestMethod()]
+        public void packTestData()
+        {
+            byte[] data = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x00 };
+            Frame f = new Frame(0x08, 0x81, data);
+
+            byte[] tmp = f.pack();
+            Assert.AreEqual(12, tmp.Length);
+            Assert.AreEqual(0x7D, tmp[(int)Frame.FIELDS.HEADER]);
+            Assert.AreEqual(0x08, tmp[(int)Frame.FIELDS.LENGTH]);
+            Assert.AreEqual(0x81, tmp[(int)Frame.FIELDS.CODE]);
+            Assert.AreEqual(0x41, tmp[(int)Frame.FIELDS.DATA + 0]);
+            Assert.AreEqual(0x44, tmp[(int)Frame.FIELDS.DATA + 3]);
+            Assert.AreEqual(0x00, tmp[(int)Frame.FIELDS.DATA + 7]);
+            Assert.AreEqual(0xC9, tmp[(int)Frame.FIELDS.DATA + f.Length]);
+        }
+
+        [TestMethod()]
+        public void constructorFromPackTestNullData()
+        {
+            Frame f = new Frame(0x00, 0x01, null);
+            byte[] tmp = f.pack();
+
+            Frame f1 = new Frame(tmp);
+            Assert.AreEqual(f, f1);
+        }
+
+        [TestMethod()]
+        public void constructorFromPackTestData()
+        {
+            byte[] data = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x00 };
+            Frame f = new Frame(0x08, 0x81, data);
+
+            byte[] tmp = f.pack();
+
+            Frame f1 = new Frame(tmp);
+            
+            Assert.AreEqual(f, f1);
+        }
+
+        [TestMethod()]
+        public void equalsTest()
+        {
+            byte[] data1 = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x00 };
+            Frame f1 = new Frame(0x08, 0x81, data1);
+
+            byte[] data2 = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x00 };
+            Frame f2 = new Frame(0x07, 0x81, data2);
+
+            byte[] data3 = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x00 };
+            Frame f3 = new Frame(0x07, 0x01, data3);
+
+            byte[] data4 = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x00 };
+            Frame f4 = new Frame(0x07, 0x01, data4);
+
+            Frame fNull = new Frame(0x00, 0x01, null);
+
+            Assert.AreNotEqual(f1, fNull);
+            Assert.AreNotEqual(f1, f2);
+            Assert.AreNotEqual(f2, f3);
+            Assert.AreEqual(f3, f4);
+        }
+
+        [TestMethod()]
+        public void isValidChecksumTest()
+        {
+            byte[] data1 = { 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x00 };
+            Frame f1 = new Frame(0x08, 0x81, data1);
+
+            byte []tmp = f1.pack();
+            Frame f2 = new Frame(tmp);
+
+            Assert.IsTrue(f2.isValidChecksum());
+
+            tmp[(int)Frame.FIELDS.DATA + f1.Length] = 0x43;
+
+            Frame f3 = new Frame(tmp);
+            
+            Assert.IsFalse(f3.isValidChecksum());
         }
     }
 }
