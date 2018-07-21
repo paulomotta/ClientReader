0bda3af8269d9e27a3f829e6c91c4440a6bd57a2
Paulo Motta <prmottajr@gmail.com>
2018-07-21 11:27:36 -0300

    Added the Leitura class that will hold data for each item read for a request. If an index is missing an object will be created and information will be added to the transaction log file

---
+++ ClientReader/ClientReader/Leitura.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientReader
+{
+    public class Leitura
+    {
+        private int indiceRegistro;
+        private string numSerie;
+        private string dataHora;
+        private float valor;
+        private bool faltante;
+        private string dataHoraLeitura;
+
+        public int IndiceRegistro
+        {
+            get
+            {
+                return indiceRegistro;
+            }
+        }
+
+        public string NumSerie
+        {
+            get
+            {
+                return numSerie;
+            }
+        }
+
+        public string DataHora
+        {
+            get
+            {
+                return dataHora;
+            }
+        }
+
+        public float Valor
+        {
+            get
+            {
+                return valor;
+            }
+        }
+
+        public bool Faltante
+        {
+            get
+            {
+                return faltante;
+            }
+        }
+
+        public string DataHoraLeitura
+        {
+            get
+            {
+                return dataHoraLeitura;
+            }
+        }
+
+        public Leitura(int indiceRegistro, string numSerie, string dataHora, float valor)
+        {
+            this.indiceRegistro = indiceRegistro;
+            this.numSerie = numSerie;
+            this.dataHora = dataHora;
+            this.valor = valor;
+            this.faltante = false;
+            this.dataHoraLeitura = (DateTime.Now).ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public Leitura(int indiceRegistro, string numSerie)
+        {
+            this.indiceRegistro = indiceRegistro;
+            this.numSerie = numSerie;
+            this.dataHora = null;
+            this.valor = 0;
+            this.faltante = true;
+            this.dataHoraLeitura = (DateTime.Now).ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
