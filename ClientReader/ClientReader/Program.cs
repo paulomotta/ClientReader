13ad4ece927d2cd0e2f9d3a0d7127d4bdf3d1aee
Paulo Motta <prmottajr@gmail.com>
2018-07-21 02:04:38 -0300

    Started reading the input file. If the file name is not informed presents an error message and terminate. If a certain line of the file starts with # ignores the line. Does not check for input line format, assuming the specification regarding single space is enforced.

--- ClientReader/ClientReader/Program.cs
+++ ClientReader/ClientReader/Program.cs
@@ -10,6 +10,25 @@ namespace ClientReader
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Por favor indique o nome do arquivo de pedidos.");
+                System.Console.WriteLine("Uso: ClientReader <nome_do_arquivo.txt>");
+                return;
+            }
+
+            string line;
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(args[0]);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.StartsWith("#")) continue; //ignorar linhas comentadas
+
+                string[] tmp = line.Split(' ');
+                System.Console.WriteLine(tmp[0]+" " + tmp[1]+ " " + tmp[2]+ " " +tmp[3]);
+            }
+
+            file.Close();
         }
     }   
 }
