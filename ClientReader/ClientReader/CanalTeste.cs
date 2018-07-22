78e4319a7ac78088cd695d8862a1c4ca5cd8c7e8
Paulo Motta <prmottajr@gmail.com>
2018-07-22 03:29:15 -0300

    Added a mock canal class that returns a Mensagem acording to what was requested, currently only LerNumSerie

---
+++ ClientReader/ClientReader/CanalTeste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientReader
+{
+    public class CanalTeste : Canal
+    {
+        Mensagem msg;
+        public override bool connect()
+        {
+            return true;
+        }
+
+        public override bool disconnect()
+        {
+            return true;
+        }
+
+        protected override Mensagem concreteReceive()
+        {
+            return Mensagem.createMensagemRespLerNumSerie("ABCDEFG\0");
+        }
+
+        protected override bool concreteSend(Mensagem cmd)
+        {
+            msg = cmd;
+            return true;
+        }
+    }
+}
