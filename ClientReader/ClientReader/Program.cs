1b81963c336b7461c13c7f8e63a02e8b007c46a7
Paulo Motta <prmottajr@gmail.com>
2018-07-22 04:56:04 -0300

    Implemented the processing of the Pedido using the CanalTeste since the server is still offline. Using the Medidor API to abstract the interaction with the remote device.

--- ClientReader/ClientReader/Program.cs
+++ ClientReader/ClientReader/Program.cs
@@ -26,13 +26,37 @@ namespace ClientReader
 
                 string[] tmp = line.Split(' ');
                 Pedido pedido = new Pedido(tmp[0], int.Parse(tmp[1]), int.Parse(tmp[2]), int.Parse(tmp[3]));
-                Canal c = new CanalSocket(pedido.Ip, pedido.Porta);
+                //Canal c = new CanalSocket(pedido.Ip, pedido.Porta);
+                Canal c = new CanalTeste();
                 Medidor medidor = new Medidor(c);
+                string numSerie = medidor.lerNumSerie();
+                UInt16 []registros = medidor.lerRegistroStatus();
 
                 for (int i = pedido.IndiceInicial; i <= pedido.IndiceFinal; i++)
                 {
+                    if (i < registros[0] || i > registros[1]) //indice inicial anterior ao que existe no medidor
+                    {
+                        Leitura leitura = new Leitura(i,numSerie);
+                        pedido.Leituras.Add(leitura);
+                    }
+                    else
+                    {
+                        if(medidor.definirIndiceLeitura((UInt16)i) ==0)
+                        {
+                            string dataHora = medidor.lerDataHoraRegistroAtual();
+                            float valor = medidor.lerValorEnergiaRegistroAtual();
+
+                            Leitura leitura = new Leitura(i, numSerie, dataHora, valor);
+                            pedido.Leituras.Add(leitura);
+                        }
+                        else
+                        {
+                            Leitura leitura = new Leitura(i, numSerie);
+                            pedido.Leituras.Add(leitura);
+                            //TODO gerar um log de registro faltante, mas dentro do intervalo
+                        }
                         
-                    medidor.executarLeitura();
+                    }
                 }
                 
             }
